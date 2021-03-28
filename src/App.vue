4203d746447464e30ba5c67286e7f2daa6130d8b
yxtwl94 <yxtwl95@hotmail.com>
2021-03-28 14:21:37 +0800

    自动图表生成

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png" alt="无法显示">
-    <div>
+    <el-container>
       <el-button @click="startHacking">Start</el-button>
       <el-button @click="testApi">test api</el-button>
+    </el-container>
+
+    <el-container>
       <el-input v-model="price" placeholder="请输入价格" @blur="get_house_title"></el-input>
-      <el-button @click="get_house_title">house title api</el-button>
-    </div>
+      <el-button @click="get_house_title">获取房源信息</el-button>
+    </el-container>
+
+    <el-container>
+      <el-main>
+        <el-table :data="houseTable">
+          <el-table-column prop="title" label="标题" width="400">
+          </el-table-column>
+          <el-table-column prop="price" label="价格" width="120">
+          </el-table-column>
+        </el-table>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
@@ -14,6 +28,7 @@
 const api_url = 'http://localhost:3000/api'
 
 export default {
+
   methods: {
     startHacking () {
       this.$notify({
@@ -35,18 +50,29 @@ export default {
       })
     },
     get_house_title(){
+      var _this = this  //直接用this会失败！
       this.$ajax.get(api_url+'/demo/get_house_title',{
         params:{
-          price:this.price, //获取价格=500的数据
+          price:this.price, //获取价格数据
         }
       }).then(function (res){
-        console.log(res.data)
+        _this.houseTable = []
+        for (var i=0;i<res.data.length;i++)
+        {
+          var item = {
+            title:res.data[i].house_title,
+            price:res.data[i].house_price
+          }
+          //console.log(item)
+          _this.houseTable.push(item)
+        }
       })
     }
   },
   data() {
     return {
-      price: ''
+      price: '',
+      houseTable:[]
     }
   }
 }
